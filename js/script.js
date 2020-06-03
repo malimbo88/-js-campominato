45ab3cf5931d4d7d7a4aa770d3b536471f774a44
Nicola <nicolavalente88@gmail.com>
2020-06-03 22:28:24 +0200

    Non ci riesco

--- js/script.js
+++ js/script.js
@@ -2,19 +2,40 @@
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 var bombe = [];
 var count = 0;
+var utente = "";
+
 while (bombe.length <= 15) {
 
-  var bomba = Math.floor(Math.random() * 16) + 1;
-  console.log("bomba " + bomba);
+  var bomba = Math.floor(Math.random() * 100) + 1;
 
   if (!(bombe.includes(bomba))) {
     bombe.push(bomba);
   }
 
-count++
 }
 
 console.log("sedici bombe " + bombe);
+
 //In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
-//Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+
+
+//Se il numero è presente nella lista dei numeri generati, la partita termina,
+// altrimenti si continua chiedendo all’utente un altro numero.
+//La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+
+while(bombe.length < 100) {
+
+  utente = prompt("Inserisci un numero");
+
+  if (!(bombe.includes(utente))) {
+    bombe.push(utente);
+    console.log("Bravo continua! Hai inserito " + utente)
+    console.log("Aggiornamento tentativi " + bombe);
+  }else {
+    console.log("Hai perso");
+    bombe.length = 100;
+  }
+
+}
+
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
